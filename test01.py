5d3fa7b18b1549924213cfc39909d6ddb2158f5e
Senadla <125400463+Senadla@users.noreply.github.com>
2023-05-28 22:00:43 -0400

    Update test01.py

--- test01.py
+++ test01.py
@@ -3,8 +3,8 @@ import unittest
 
 class CalculatorTests(unittest.TestCase):
     def test_addition(self):
-        result = 3 + 3
-        self.assertEqual(result, 6)
+        result = 3 + 7
+        self.assertEqual(result, 10)
 
     def test_multiplication(self):
         result = 2 * 3
