190f87fc5d8cb1195baef82e33c8ee13e8cc8d35
Senadla <125400463+Senadla@users.noreply.github.com>
2023-05-28 21:49:50 -0400

    Update test01.py

--- test01.py
+++ test01.py
@@ -1,1 +1,13 @@
+import unittest
 
+class CalculatorTests(unittest.TestCase):
+    def test_addition(self):
+        result = 2 + 3
+        self.assertEqual(result, 5)
+
+    def test_multiplication(self):
+        result = 2 * 3
+        self.assertEqual(result, 6)
+
+if __name__ == '__main__':
+    unittest.main()
